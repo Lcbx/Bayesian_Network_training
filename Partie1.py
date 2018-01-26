c6198e93cebbcbaefa707fef6c0753a83f31910d
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2018-01-25 20:58:13 -0500

    better print statements

--- Partie1.py
+++ Partie1.py
@@ -1,17 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#lesarrayssontbatisaveclesdimensionssuivantes:
-#pluie,arroseur,watson,holmes
-#etchaquedimension:faux,vrai
+# les arrays sont batis avec les dimensions suivantes:
+# pluie, arroseur, watson, holmes
+# et chaque dimension : faux, vrai
+
 prob_pluie=np.array([0.8,0.2]).reshape(2,1,1,1)
-print("Pr(Pluie)={}\n".format(np.squeeze(prob_pluie)))
+
+print("Pr(Pluie)=\n" + str(np.squeeze(prob_pluie)))
+
+
 prob_arroseur=np.array([0.9,0.1]).reshape(1,2,1,1)
-print("Pr(Arroseur)={}\n".format(np.squeeze(prob_arroseur)))
+
+print("Pr(Arroseur)=\n" + str(np.squeeze(prob_arroseur)))
+
+
+
 watson=np.array([[0.8,0.2],[0,1]]).reshape(2,1,2,1)
-print("Pr(Watson|Pluie)={}\n".format(np.squeeze(watson)))
+
+print("Pr(Watson|Pluie)=\n" + str(np.squeeze(watson)))
+
 # TODO: determiner facteurs Holmes 
-holmes=np.array([[0.8,0.2],[0,1]]).reshape(2,2,1,2)
-print("Pr(Holmes|Pluie,arroseur)={}\n".format(np.squeeze(holmes)))
+
+#holmes=np.array([[0.8,0.2],[0,1]]).reshape(2,2,1,2)
+
+#print("Pr(Holmes|Pluie,arroseur)={}\n".format(np.squeeze(holmes)))
+
+
 watson[0,:,1,:]
 (watson*prob_pluie).sum(0).squeeze()[1]
 #holmes[0,1,0,1]
