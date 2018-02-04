376200b77f18795a1c6c0e1d39f3b27692761310
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2018-02-04 12:03:23 -0500

    fini

--- Partie1.py
+++ Partie1.py
@@ -1,5 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+
+
+
+
 # les arrays sont batis avec les dimensions suivantes:
 # pluie, arroseur, watson, holmes
 # et chaque dimension : faux, vrai
@@ -8,24 +14,40 @@ prob_pluie=np.array([0.8,0.2]).reshape(2,1,1,1)
 
 print("Pr(Pluie)=\n" + str(np.squeeze(prob_pluie)))
 
-
 prob_arroseur=np.array([0.9,0.1]).reshape(1,2,1,1)
 
 print("Pr(Arroseur)=\n" + str(np.squeeze(prob_arroseur)))
 
-
+print
 
 watson=np.array([[0.8,0.2],[0,1]]).reshape(2,1,2,1)
 
-print("Pr(Watson|Pluie)=\n" + str(np.squeeze(watson)))
+print("Pr(Watson | Pluie)=\n" + str(np.squeeze(watson)))
+
+print
 
 # TODO: determiner facteurs Holmes 
 
-#holmes=np.array([[0.8,0.2],[0,1]]).reshape(2,2,1,2)
+holmes=np.array([[[1,0],[0.1,0.9]],[[0,1],[0,1]]]).reshape(2,2,1,2)
 
-#print("Pr(Holmes|Pluie,arroseur)={}\n".format(np.squeeze(holmes)))
+print("Pr(Holmes | Pluie, arroseur)=\n" + str(np.squeeze(holmes)))
 
+print
 
-watson[0,:,1,:]
-(watson*prob_pluie).sum(0).squeeze()[1]
-#holmes[0,1,0,1]
+# Pr(H=1)
+prH = np.squeeze((holmes * prob_pluie * prob_arroseur) .sum(0).sum(0) )[1]
+print("Pr(Holmes=1)= " + str(prH))
+
+print
+
+# Pr(A=1)
+prA = prob_arroseur[:,1,:,:]
+# Pr(A=1|H=1) = Pr(H=1|A=1) * Pr(A=1) / Pr(H=1)
+prAH = np.squeeze( np.squeeze(( holmes * prob_pluie ) .sum(0)[1])[1]  *prA/prH )
+print("Pr(Arroseur=1 | Holmes=1) = " + str(np.squeeze( prAH ) ))
+
+print
+
+# Pr(A = 1|H = 1, W = 1)
+print("Pr(Arroseur=1 | Holmes=1, Watson =1) = " + str(prAH) )
+
--- Partie2.py
+++ Partie2.py
@@ -13,59 +13,101 @@ y_one_hot=np.zeros((y.shape[0], len(np.unique(y))))
 y_one_hot[np.arange(y.shape[0]), y] = 1 #one hot target or shape NxK
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size=0.3, random_state=42) #cuts dataset into trainingset (70%), rest (30%)
 
-X_test, X_validation,y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5,
-random_state=42)
+X_test, X_validation,y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5, random_state=42) #cuts rest into testset (15%), validationset(15%)
 
-
-W=np.random.normal(0,0.01,(len(np.unique(y)),X.shape[1])) # weights of shape KxL
-
-best_W = None
-best_accuracy = 0
-lr = 0.001
-nb_epochs = 500
-minibatch_size = len(y)//20
-
-losses=[]
-accuracies=[]
 
 def softmax(x):
-	# assurez vous que la fonction est numeriquement stable
-	#e.g.softmax(np.array([1000,10000,100000],ndim=2))
-	pass
+	 result = np.exp(x) / float(np.sum(np.exp(x)) + 1e-9)
+	 return result
+	 
+def fprop(X,W):
+	return softmax(np.dot(X, W.T))
+	
 	
 def get_accuracy(X,y,W):
-	pass
+	y_pred = np.argmax(fprop(X,W), axis = 1)
+	y_t = np.argmax(y, axis = 1)
+	accuracy = np.mean(y_pred==y_t)
+	return accuracy
 
 def get_grads(y,y_pred,X):
-	pass
+	return np.dot((y - y_pred).T, X)
 
 def get_loss(y,y_pred):
-	pass
+	return -1.0 * np.log(np.sum(y * y_pred))
 
-for epoch in range(nb_epochs):
+def experiment(batch_size = 128, learning_rate = 0.0000003):
 	
-	loss=0
-	accuracy=0
+	W=np.random.normal(0,0.01,(len(np.unique(y)),X.shape[1])) # weights of shape KxL
 	
-	for i in range(0,X_train.shape[0], minibatch_size):
-		pass #TODO
+	best_W = None
+	best_accuracy = 0
 	
-	losses.append(loss) # compute the loss on the train set
-	accuracy=None #TODO
-	accuracies.append(accuracy)# compute the accuracy on the validation set
-	
-	if accuracy > best_accuracy:
-		pass # select the best parameters based on the validation accuracy
+	nb_epochs = 500
+	minibatch_size = batch_size
+
+	losses_validation=[]
+	losses_train=[]
+
+	for epoch in range(nb_epochs):
+
+		for i in range (0 , X_train . shape [ 0 ] , minibatch_size ):
+			
+			minibatch_x = np.array(X_train[i:i+minibatch_size])
+			minibatch_y = np.array(y_train[i:i+minibatch_size])
+
+			y_pred = fprop(minibatch_x,W) 
+			delta  = lr * get_grads(minibatch_y, y_pred, minibatch_x)
+			W += delta
+			
+		# compute the losses
 		
-	accuracy_on_unseen_data = get_accuracy(X_test,y_test,best_W)
-	
-	print(accuracy_on_unseen_data)#0.897506925208
+		y_pred = fprop(X_validation,W)
+		loss_validation = get_loss(y_validation, y_pred)
+		#print "loss_validation " + str(loss_validation)
+		losses_validation.append(loss_validation) 
+		
+		y_pred = fprop(X_train,W)
+		loss_train= get_loss(y_train, y_pred)
+		#print "loss_train " + str(loss_train)
+		losses_train.append(loss_train) 
+		
+		
+		# compute the accuracy on the validation set
+		accuracy=get_accuracy(X_validation, y_validation, W)
+		#print "accuracy after batch : " + str(accuracy)
+		
+		# select the best parameters based on the validation accuracy
+		if accuracy > best_accuracy:
+			best_accuracy = accuracy
+			best_W = np.copy(W) 
+				
+	accuracy_on_unseen_data = get_accuracy(X_test,y_test, best_W)
 
-	plt.plot(losses)
-	plt.imshow(best_W[4,:].reshape(8,8))
+	#print "accuracy on validation : " + str(accuracy_on_unseen_data) #0.897506925208
+
+	#print "batch size  : " + str(minibatch_size) + " | learning rate : " + str(lr)
+
+	plt.title("Batch size  : " + str(minibatch_size) + "  & Learning rate : " + str(lr) + " (best accuracy : " + str(best_accuracy)[:4] + ") ")
+	plt.ylabel("Average negative log likelihood")
+	plt.xlabel("Epoch")
+	plt.plot(losses_train, label="train")
+	plt.plot(losses_validation, label="validation")
+	plt.legend()
+	plt.savefig("bs"+str(minibatch_size)+"lr"+ str(lr) + ".png") #plt.show()
+	plt.close()
 	
+
+batch_sizes = [32, 96, 256]
+learning_rates = [0.00001, 0.000001, 0.0000001]	
+
+for size in batch_sizes:
+	for lr in learning_rates:
+		experiment(size, lr)
+
+
 	
 	
 	
